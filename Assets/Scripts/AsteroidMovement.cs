61186ae9eefcb38598d3b44b2920ae78341c2e60
oza2000 <owen.aguirre@gmail.com>
2017-11-21 20:56:39 -0800

    Stopping point 11-21 9:00

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class AsteroidMovement : MonoBehaviour {
 
+	private Rigidbody2D Rb;
+
+	public float speed;
+
 	// Use this for initialization
 	void Start () {
-		
+		Rb = GetComponent<Rigidbody2D> ();
+		Rb.velocity = transform.right * speed;
 	}
 	
 	// Update is called once per frame
